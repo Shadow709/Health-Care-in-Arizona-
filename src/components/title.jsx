4f578a9e6b70acfa4aa485dae50150dc502c768b
amir <nintendothing123@gmail.com>
2025-01-25 13:54:51 -0800

    added components

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Title({ text }) {
-  return <div className="text-7xl">{text}</div>;
+  return <div className="text-7xl text-slate-900">{text}</div>;
 }
